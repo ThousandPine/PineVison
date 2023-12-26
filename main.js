38088d5494cc560059e61b2d76b7827cc045964a
Pine <2277367263@qq.com>
2023-12-26 21:32:32 +0800

    update: 调整默认窗口尺寸

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const addons = require('./build/Release/addon')
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 750,
         minWidth: 600,
         minHeight: 600,
         webPreferences: {
