2619e40e3f67b1406fd45cfb3ca6799ce7d6f275
Pine <2277367263@qq.com>
2023-12-30 11:04:26 +0800

    chore: 修复打包后addon加载失败的问题
    在packageAfterExtract阶段添加hook把opencv的dll复制到根目录下

--- forge.config.js
+++ forge.config.js
@@ -3,6 +3,15 @@ module.exports = {
     asar: true,
   },
   rebuildConfig: {},
+  hooks: {
+    packageAfterExtract: async (config, buildPath, electronVersion, platform, arch) => {
+      const path = require('path');
+      const fs = require('fs-extra');
+      const src = 'D:/A/OpenCV/opencv/build/x64/vc15/bin/opencv_world454.dll';
+      const dest = path.join(buildPath, 'opencv_world454.dll');
+      await fs.copy(src, dest);
+    }
+  },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
